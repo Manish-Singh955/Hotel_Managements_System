96a4d176faaee9484fb1df1e9505929ce4042f2a
Manish-Singh955 <manishkusingh35@gmail.com>
2025-03-26 20:20:46 +0530

    Add files via upload

---
+++ fi.php
@@ -0,0 +1,38 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home (Hotel Management)</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link href="img/LOGO.png" type="img/icon" rel="icon">
+</head>
+<body>
+	<div id="full">
+		<div id="bg" style="background-image: url('img/login.avif'); height: 1200px;">
+		<div id="header">
+			<div id="logo">
+				<h1><font color="white">M$ HOTEL</font></h1>
+			</div>
+			<div id="nav">
+				<ul>
+					<li><a href="index.php">Home</a></li>
+					<li><a href="contect.php">Contect Us</a></li>
+					<li><a href="bms.php">BOOK MY STAY</a></li>
+					<li><a href="#">Our Hotel</a></li>
+					<li><a href="#">Help</a></li>
+				</ul>
+			</div>
+		</div>
+		<div id="banner">
+			<div id="form"><br><br><br><br><br><br><br><br><br><br><br><br>
+				<h1 style="text-align: center;color: white;">   REGISTRATION COMPLETED</h1>
+				<h4 style="text-align: center;color: white;">THANK YOU FOR VISTING ON WEBSITE</h4>
+					</div>
+		</div>
+	</div>
+	</div>
+	
+  </div>
+</body>
+
+</html>
